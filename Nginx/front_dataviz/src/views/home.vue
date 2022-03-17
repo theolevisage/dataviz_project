f379948768ee60465daa427a729f3eaf8361d6ff
big_hang <big_hang@pop-os.localdomain>
2022-03-17 16:59:55 +0100

    joe

--- Nginx/front_dataviz/src/views/home.vue
+++ Nginx/front_dataviz/src/views/home.vue
@@ -1,18 +1,15 @@
 <template>
     <div class="flex justify-center h-full">
       <div class="inline-flex justify-center flex-wrap h-screen m-4">
-        <random-chart></random-chart>
       </div>
     </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
-import RandomChart from "../components/RandomChart.vue";
 
 export default defineComponent({
-  name: 'Home',
-  components: {RandomChart}
+  name: 'Home'
 })
 </script>
 
