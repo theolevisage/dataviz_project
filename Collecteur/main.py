6770ba0d4ea9608b17ec0cc7ab39dfdf8694c860
Maxldev <maximeleraydev@gmail.com>
2022-03-18 11:34:28 +0100

    alternate way docker compose / unite prod / api

--- Collecteur/main.py
+++ Collecteur/main.py
@@ -1,26 +1,56 @@
+import socket
+import os
+from _thread import *
+ServerSideSocket = socket.socket()
+host = '172.20.0.10'
+port = 65432
+ThreadCount = 0
+try:
+    ServerSideSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+print('Socket is listening..')
+ServerSideSocket.listen(5)
+def multi_threaded_client(connection):
+    connection.send(str.encode('Server is working:'))
+    while True:
+        data = connection.recv(2048)
+        response = 'Server message: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(response))
+    connection.close()
+while True:
+    Client, address = ServerSideSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(multi_threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSideSocket.close()
+
 # This is a sample Python script.
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from time import sleep
-import socket
+# from time import sleep
+# import socket
 
-HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+# HOST = "172.20.0.11"  # Standard loopback interface address (localhost)
+# PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 
-if __name__ == '__main__':
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    conn.sendall(data)
+# if __name__ == '__main__':
+#     while True:
+#         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#             s.bind((HOST, PORT))
+#             s.listen()
+#             conn, addr = s.accept()
+#             with conn:
+#                 print(f"Connected by {addr}")
+#                 while True:
+#                     data = conn.recv(1024)
+#                     if not data:
+#                         break
+#                     conn.sendall(data)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- UniteProd/main.py
+++ UniteProd/main.py
@@ -1,29 +1,49 @@
+import socket
+ClientMultiSocket = socket.socket()
+host = '172.20.0.10'
+port = 65432
+print('Waiting for connection response')
+try:
+    ClientMultiSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+res = ClientMultiSocket.recv(1024)
+while True:
+    Input = input('Hey there: ')
+    ClientMultiSocket.send(str.encode(Input))
+    res = ClientMultiSocket.recv(1024)
+    print(res.decode('utf-8'))
+ClientMultiSocket.close()
+
+
+
+
 # This is a sample Python script.
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from time import sleep
-import socket
-import json
+# from time import sleep
+# import socket
+# import json
 
-HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+# HOST = "172.20.0.10"  # Standard loopback interface address (localhost)
+# PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-m = b'{"id": 2, "name": "abc"}'
-jsonObj = json.loads(m)
-data = jsonObj
+# m = b'{"id": 2, "name": "abc"}'
+# jsonObj = json.loads(m)
+# data = jsonObj
 
-if __name__ == '__main__':
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            print("BEFORE CONNECT")
-            s.connect((HOST, PORT))
-            print("AFTER CONNECT")
-            s.sendall(m)
-            received = s.recv(1024)
+# if __name__ == '__main__':
+#     while True:
+#         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#             print("BEFORE CONNECT")
+#             s.connect(("172.20.0.10", 65432))
+#             print("AFTER CONNECT")
+#             s.sendall(m)
+#             received = s.recv(1024)
 
-        print(f"Sent:     {json.loads(m)}")
-        print(f"Received: {json.loads(received)}")
-        sleep(5)
+#         print(f"Sent:     {json.loads(m)}")
+#         print(f"Received: {json.loads(received)}")
+#         sleep(5)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
