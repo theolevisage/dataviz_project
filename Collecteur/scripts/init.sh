0417287a1c2fbe81a6134d35bc29ab02fc02bcd1
theolevisage <theo.levisage@tutanota.com>
2022-04-08 10:40:59 +0200

    update keygen in init.sh and add workdir and update paths in Dockerfile

--- Collecteur/scripts/init.sh
+++ Collecteur/scripts/init.sh
@@ -1,10 +1,11 @@
 apt update -y
 which gpg || apt install gpg -y
-FILE=./public.pgp
+mkdir /.keys
+FILE=/.keys/public.pgp
 if test -f "$FILE"; then
     echo "$FILE exists."
 else
     echo "$FILE does not exist."
     gpg --batch --gen-key keygen-batch
-    gpg --output public.pgp --armor --export mail@example.com
+    gpg --output /.keys/public.pgp --armor --export mail@example.com
 fi
