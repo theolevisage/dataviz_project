70a4cd464e0eeec1aff8303d5df6a7d4da53e86d
theolevisage <theo.levisage@tutanota.com>
2022-04-08 17:13:02 +0200

    donne tout allmighto

--- Collecteur/scripts/init.sh
+++ Collecteur/scripts/init.sh
@@ -1,14 +1,15 @@
 apt update -y
 which gpg || apt install gpg -y
 mkdir /.keys
+mkdir /batch
 FILE=/.keys/public.pgp
 if test -f "$FILE"; then
     echo "$FILE exists."
 else
     echo "$FILE does not exist."
-    export name=$1
-    export mail=$2
-    envsubst < keygen-batch > updated-keygen-batch
-    gpg --batch --gen-key updated-keygen-batch
-    gpg --output /.keys/public.pgp --armor --export mail@example.com
+    export NAME=$1
+    export MAIL=$2
+    envsubst < keygen-batch > /batch/updated-keygen-batch
+    gpg --batch --gen-key /batch/updated-keygen-batch
+    gpg --output /.keys/public.pgp --armor --export $2
 fi
